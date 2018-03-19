2ec732df48f371adddcb4d92bac0c9e02bd57662
cpochard <camille.poc@gmail.com>
2018-03-19 16:10:42 +0100

    Rajout d'une exception.

--- src/com/cpochard/Program.java
+++ src/com/cpochard/Program.java
@@ -1,10 +1,15 @@
 package com.cpochard;
 
+import java.io.File;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		}
 	}
-
 }
